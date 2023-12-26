52eecfd022e8d73fa72a45572f4f9589c6626aa2
ChalcCHAK <143936501+ChalcCHAK@users.noreply.github.com>
2023-12-26 19:04:20 +0300

    food

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const tabs = document.querySelectorAll('.tabheader__item');
-const tabContent = document.querySelectorAll('.tabcontent');
+const tabContent = document.querySelectorAll ('.tabcontent');
 const tabParent = document.querySelector('.tabheader');
 
 function hideTabContent(){
@@ -68,9 +68,9 @@ document.addEventListener('keydown',(event)=>{
 })
 
 //slider 
-const slides = document.addEventListener('.offer__slider'),
-prev = document.querySelector('.offer__slider-prev'),
-next = document.querySelector('.offer__slider-next'),
+const slides = document.addEventListener('.offer__slide'),
+prev = document.querySelector('.offer__slide-prev'),
+next = document.querySelector('.offer__slide-next'),
 total = document.querySelector('#total'),
 current = document.querySelector('#current');
 
@@ -111,11 +111,48 @@ prev.addEventListener('click',()=>{
     plusSlides(-1)
 })
 
-//time
 
-const timeEnd = '2024-02-19',
-days = document.querySelector('#days'),
-hours = document.querySelector('#hours'),
-minutes = document.querySelector('#minutes'),
-seconds = document.querySelector('#second'),
-timer = document.querySelector('.timer');
+
+//timemer 
+
+const timeEnd = '2024-02-19';
+
+function getTimeRemaining(endtime){
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t/(1000*60*60*24)),
+        hours = Math.floor((t/(1000*60*60))%24),
+        minutes = Math.floor((t/(1000*60*60))%60),
+        seconds =  Math.floor((t/(1000))%60);
+    return{
+        'total':t,
+        'days':days,
+        'hours': hours,
+        'minutes':minutes,
+        'seconds':seconds
+    }
+          
+}
+function setClock(selector, endtime){
+    const timer = document.querySelector(selector),
+        days = document.querySelector('#days'),
+        hours = document.querySelector('#hours'),
+        minutes = document.querySelector('#minutes'),
+        seconds = document.querySelector('#seconds');
+        timeInterval = setInterval(updateClock,1000);
+    updateClock();
+    function updateClock(){
+        const t = getTimeRemaining(endtime);
+        days.innerHTML = t.days;
+        hours.innerHTML = t.hours;
+        minutes.innerHTML = t.minutes;
+        seconds.innerHTML = t.seconds;
+        if(t.total<=0){
+            clearInterval()
+            days.innerHTML = "00";
+            hours.innerHTML = "00";
+            minutes.innerHTML = "00";
+            seconds.innerHTML = "00";
+        }
+    }
+}
+updateClock('.timer',timeEnd);
